c86342d48957ff4d57574c8c46dd48c8b1e14ee9
losout0 <leandro.oliveira.souto@ccc.ufcg.edu.br>
2024-09-10 15:20:25 -0300

    serial não serial

--- lab4/go/serial/sum.go
+++ lab4/go/serial/sum.go
@@ -6,6 +6,11 @@ import (
 	"os"
 )
 
+type Tuple struct{
+	_sum int
+	path string
+}
+
 // read a file from a filepath and return a slice of bytes
 func readFile(filePath string) ([]byte, error) {
 	data, err := ioutil.ReadFile(filePath)
@@ -17,18 +22,19 @@ func readFile(filePath string) ([]byte, error) {
 }
 
 // sum all bytes of a file
-func sum(filePath string) (int, error) {
+func sum(filePath string, out chan Tuple) {
 	data, err := readFile(filePath)
-	if err != nil {
-		return 0, err
-	}
+
+	if err == nil{}
 
 	_sum := 0
 	for _, b := range data {
 		_sum += int(b)
 	}
+	
+	dado := Tuple{_sum, filePath}
 
-	return _sum, nil
+	out <- dado
 }
 
 // print the totalSum for all files and the files with equal sum
@@ -40,16 +46,21 @@ func main() {
 
 	var totalSum int64
 	sums := make(map[int][]string)
+	info := make(chan Tuple)
 	for _, path := range os.Args[1:] {
-		_sum, err := sum(path)
+		//_sum, err := sum(path)
+		go sum(path, info)
 
-		if err != nil {
-			continue
-		}
+		//if err != nil {
+		//	continue
+		//}
+		//sums[_sum] = append(sums[_sum], path)
+	}
 
-		totalSum += int64(_sum)
-
-		sums[_sum] = append(sums[_sum], path)
+	for i := 0; i < len(os.Args[1:]); i++ {
+		val := <- info
+		totalSum += int64(val._sum)
+		sums[val._sum] = append(sums[val._sum], val.path)
 	}
 
 	fmt.Println(totalSum)
@@ -59,4 +70,5 @@ func main() {
 			fmt.Printf("Sum %d: %v\n", sum, files)
 		}
 	}
+
 }
